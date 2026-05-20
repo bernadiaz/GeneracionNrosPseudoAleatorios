30fda80b2e59bcc6f77ceab7b4eaa9a0f5e340c8
Angeles Navarro Zavalia <navarroanguie5@gmail.com>
2026-05-20 18:02:17 -0300

    todo integrado en pruebas, incluyendo las nuevas

--- src/pruebas.java
+++ src/pruebas.java
@@ -6,6 +6,8 @@ public class pruebas {
         pruebasPromedio pProm = new pruebasPromedio();
         pruebasFrecuencia pFrec = new pruebasFrecuencia();
         pruebasSeries pSerie = new pruebasSeries();
+        pruebasKS pKS = new pruebasKS();
+        pruebasCorrida pCorrida = new pruebasCorrida();
 
         if(pProm.pruebaPromedio(datos)){
             System.out.println("Aprobo prueba de prom");
@@ -19,6 +21,15 @@ public class pruebas {
             System.out.println("Aprobo prueba de serie");
             return true;
         }
+        if(pKS.logicaPruebaKS(datos)){
+            System.out.println("Aprobo prueba KS");
+            return true;
+        }
+        if(pCorrida.logicaPruebaCorrida(datos)){
+            System.out.println("Aprobo prueba corrida");
+            return true;
+        }
+
 
         return false;
     }
